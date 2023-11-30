3761fb7ee77ba0eb63ad787900191a3ecce08ae7
Siyanbola Akin <akin.siyanbola@gmail.com>
2023-11-30 08:48:18 +0100

    set up ios

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,10 +1,36 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // startTimer();
+  }
+
+  // startTimer() async {
+  //   var duration = const Duration(seconds: 5);
+  //   return Timer(duration, route);
+  // }
+
+  // route() {
+  //   Navigator.of(context)
+  //       .pushReplacementNamed(AppRoute.onboarding //AppRoute.nav,
+  //           );
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.yellow,
+    );
   }
 }
